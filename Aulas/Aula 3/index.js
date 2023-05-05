95afdd8b1ce2da7cbb742f1affe2d97b070042f8
Vinicius-CSantos <vinicius_correa.santos@hotmail.com>
2023-05-05 09:43:53 -0300

    First four classes

---
+++ Aulas/Aula 3/index.js
@@ -0,0 +1,8 @@
+console.log('Vinicius');
+console.log("Vinicius 'Correa'");
+console.log(`Vinicius correa`);
+
+//Tudo acima é considerado String.
+// String = Texto
+
+console.log(35, 15.85, "Vinicius Correa");
